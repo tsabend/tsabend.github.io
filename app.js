36fa6b0f55c526a966815dec5a4bafaf98fd8e40
Thomas Abend <tsabend@gmail.com>
2014-12-03 12:38:38 -0500

    Edited flipper colors

--- app.js
+++ app.js
@@ -1,4 +1,8 @@
 $(document).ready(function(){ 
+	var resume = "<h3>RESUME</h3><p>I am a person</p>"
+	var projects = "<h3>PROJECTS</h3><p>Bibliotate: A literacy app that let's students comment directly in the text.</p><p>VYNC: A video messaging app that lets users create chains of videos.</p>"
+	var aboutMe = "<h3>ABOUT ME</h3><p>I am a former teacher turned web developer. I'm originally from Massachusetts and am now living in Brooklyn. I have spent the past four years working as an 8th grade English teacher in the Mississippi Delta and Newark, New Jersey. I'm transitioning into programming with an eye on education technology.<p>Besides coding, I enjoy playing music, cooking, and fencing.</p>"
+	
 	$('.flip-container').click(function() {
 		var selected = $(this).data('text')
 		var box = $('.text-info')
@@ -14,7 +18,3 @@ $(document).ready(function(){
 	})
 });
 
-
-var resume = "<h3>RESUME</h3><p>I am a person</p>"
-var projects = "<h3>PROJECTS</h3><p>Bibliotate: A literacy app that let's students comment directly in the text.</p><p>VYNC: A video messaging app that lets users create chains of videos.</p>"
-var aboutMe = "<h3>ABOUT ME</h3><p>I am a former teacher turned web developer. I'm originally from Massachusetts and am now living in Brooklyn. I have spent the past four years working as an 8th grade English teacher in the Mississippi Delta and Newark, New Jersey. I'm transitioning into programming with an eye on education technology.<p>Besides coding, I enjoy playing music, cooking, and fencing.</p>"
