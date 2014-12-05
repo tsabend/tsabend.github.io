4607d2c402b95320d3613b5623c97d40342a9b5a
Thomas Abend <tsabend@gmail.com>
2014-12-05 09:49:26 -0500

    Added an iframe for resume viewing as pdf

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){ 
-	var resume = "<h3>RESUME</h3><p>I am a person</p>"
+	var resume = '<button class="resume-button mb1">x</button><iframe class="resume" src="images/tabend_resume.pdf"/>'
 	var projects = "<h3>PROJECTS</h3><p>Bibliotate: A literacy app that let's students comment directly in the text.</p><p>VYNC: A video messaging app that lets users create chains of videos.</p>"
 	var aboutMe = "<h3>ABOUT ME</h3><p>I am a former teacher turned web developer. I'm originally from Massachusetts and am now living in Brooklyn. I have spent the past four years working as an 8th grade English teacher in the Mississippi Delta and Newark, New Jersey. I'm transitioning into programming with an eye on education technology.<p>Besides coding, I enjoy playing music, cooking, and fencing.</p>"
 	
@@ -13,6 +13,14 @@ $(document).ready(function(){
 			box.html(projects)
 		} else if (selected === "resume") {
 			box.html(resume)
+			$(document).keyup(function(e){
+				if(e.keyCode == 27) {
+					box.html("")
+				}
+			})
+			$('.resume-button').click(function(){
+				box.html("")
+			})
 		}
 
 	})
